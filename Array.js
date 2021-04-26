620d7e2eff8e82a5a91f8a88ca5fee9259d67b57
Chad Eckles <chad1020@gmail.com>
2021-04-26 15:52:28 -0500

    array continue

--- Array.js
+++ Array.js
@@ -13,7 +13,14 @@ console.log(selectedColors);
 //selectedColors.splice(3,1); //remove item 3 is loaction 1 is amount of items to remove
 
 console.log(selectedColors.filter((item) => typeof item !== "number")); //filter array to only show Strings
+console.log(selectedColors.includes("Red")); //T/F is pressenct
 console.log(selectedColors.length);
 console.log(selectedColors[0]);
 selectedColors[1]= "Purple";
 console.log(selectedColors);
+console.log(selectedColors.indexOf("Green") + " Index of");
+selectedColors.forEach((item) => console.log(item)); //short way
+
+for( let i = 0; i < selectedColors.length; i++){
+    console.log(selectedColors[i]);
+} //Long way
