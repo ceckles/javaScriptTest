f4a2dac78eff9a60d86ceb54f6076c8cf881b836
Chad Eckles <chad1020@gmail.com>
2021-04-26 15:35:58 -0500

    more array edits.

--- Array.js
+++ Array.js
@@ -1,10 +1,19 @@
-let selectedColors = ['red', 'blue'];
-selectedColors[2] = 'green';
+let selectedColors = ['Red', 'Blue'];
+selectedColors[2] = 'Green';
 selectedColors[3] = 1;
 selectedColors[4] = "A";
+selectedColors.push("Orange"); // add to end of array
 console.log(selectedColors);
+selectedColors.unshift("Yellow"); //add to begging of array
+console.log(selectedColors);
+selectedColors.shift(); //removed first item in array
+console.log(selectedColors);
+selectedColors.pop(); //remove last item
+console.log(selectedColors);
+//selectedColors.splice(3,1); //remove item 3 is loaction 1 is amount of items to remove
+
 console.log(selectedColors.filter((item) => typeof item !== "number")); //filter array to only show Strings
 console.log(selectedColors.length);
 console.log(selectedColors[0]);
-selectedColors[1]= "purple";
+selectedColors[1]= "Purple";
 console.log(selectedColors);
