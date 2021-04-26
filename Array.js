036ce1b120a6e14a16788e609759fde37d859b5e
Chad Eckles <chad1020@gmail.com>
2021-04-26 15:11:04 -0500

    Array testing.

--- Array.js
+++ Array.js
@@ -3,5 +3,8 @@ selectedColors[2] = 'green';
 selectedColors[3] = 1;
 selectedColors[4] = "A";
 console.log(selectedColors);
-console.log(selectedColors[0]);
+console.log(selectedColors.filter((item) => typeof item !== "number")); //filter array to only show Strings
 console.log(selectedColors.length);
+console.log(selectedColors[0]);
+selectedColors[1]= "purple";
+console.log(selectedColors);
